44a8f7d3159e293938a59f8d725375a02222f4c5
Gabriel <gabrielcabeca26@gmail.com>
2021-03-14 23:40:57 -0300

    tests xpto

--- src/views/tables/oportTables/parcelaAtrasadaTable.js
+++ src/views/tables/oportTables/parcelaAtrasadaTable.js
@@ -138,6 +138,9 @@ class ParcelaAtrasadaTable extends Component {
       if (a.Cliente > b.Cliente) {
         return 1;
       }
+      return 0;
+    }
+    function compare1(a, b) {
       if (a.Oportunidade.cod < b.Oportunidade.cod) {
         return -1;
       }
@@ -147,7 +150,7 @@ class ParcelaAtrasadaTable extends Component {
       return 0;
     }
     this.setState({
-      data: data.sort(compare)
+      data: data.sort(compare, compare1)
     });
   };
 
