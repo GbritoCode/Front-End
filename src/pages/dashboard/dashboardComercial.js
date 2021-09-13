485ed5b05d8a7c18b4f2da34558194aaa4f59596
Gabriel <gabrielcabeca26@gmail.com>
2021-09-13 14:10:56 -0300

    tentando acertar alinhamento de graficos na dash

--- src/pages/dashboard/dashboardComercial.js
+++ src/pages/dashboard/dashboardComercial.js
@@ -718,7 +718,7 @@ export default function ComercialDashboard() {
                       <i className="tim-icons icon-send text-info" />{" "}
                     </CardTitle>
                   </CardHeader>
-                  <CardBody style={{ paddingBottom: "0.8%" }}>
+                  <CardBody style={{ padding: "1.35%" }}>
                     <div className="chart-area">
                       <Bar
                         data={barChart_1.data(
